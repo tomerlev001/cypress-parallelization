e480f52a3ff9486297b9066cae402e4edc3124ef
Tomer Lev <tomerlev001@gmail.com>
2023-12-11 01:17:17 +0200

    Change the config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "kbzwcx",
+    projectId: "ir4cef",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
